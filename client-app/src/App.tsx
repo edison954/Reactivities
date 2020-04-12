8d18f679912ee428e0222fc71c98d9828823fde3
Edison Arley Plaza Marin <edison954@gmail.com>
2020-04-12 12:41:19 -0500

    actualiza con cambios de la seccion 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { cars } from "./demo";
-import { CarItem } from "./CarItem";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        {cars.map(car => (
-          <CarItem car={car} />
-        ))}
-      </ul>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h2" icon="users" content="Reactivities" />
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default App;
